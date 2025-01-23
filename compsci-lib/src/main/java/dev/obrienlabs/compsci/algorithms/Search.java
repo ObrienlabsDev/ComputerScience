3db40df54c72cd4a8cf5351fe882109efd3e7f55
Michael OBrien <michael@obrienlabs.org>
2025-01-23 16:09:55 -0500

    #2 - left to right vis problem p2

--- compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Search.java
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Search.java
@@ -1,5 +1,9 @@
 package dev.obrienlabs.compsci.algorithms;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Search {
 
 	/**
@@ -10,5 +14,23 @@ public class Search {
 		int[] input =  {4, 3, 2, 3, 1};
 		int[] output = {1,0,0,0,1};
 		
+		// start iterating from left to right
+		// compare the current index with the sublist on the right - it must be GT
+		// for in place - mark the index as 1 if GT, 0 if equal or LT all to the right
+		// Will involve a 2 level nested search
+		// O(n) time complexity
+		// O(1) storage complexity
+		
+		// for 1 pass
+		List<Integer> list = Arrays.stream(input)
+				.boxed() // int to Integer
+				.collect(Collectors.toList());
+		list.forEach(x -> System.out.println(x));
+
+	}
+	
+	public static void main(String[] args) {
+		Search search = new Search();
+		search.determineLeftToRightGlobalOrder();
 	}
 }
