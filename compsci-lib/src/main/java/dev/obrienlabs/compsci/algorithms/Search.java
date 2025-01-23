06615e5d74a00bab9647fd04fa0d0fb1fd61d4fc
Michael OBrien <michael@obrienlabs.org>
2025-01-23 12:22:58 -0500

    #2 - left to right vis problem

---
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Search.java
@@ -0,0 +1,14 @@
+package dev.obrienlabs.compsci.algorithms;
+
+public class Search {
+
+	/**
+	 * 20250123
+	 * Compute whether each item is the largest item between itself and all other items on the right
+	 */
+	public void determineLeftToRightGlobalOrder() {
+		int[] input =  {4, 3, 2, 3, 1};
+		int[] output = {1,0,0,0,1};
+		
+	}
+}
