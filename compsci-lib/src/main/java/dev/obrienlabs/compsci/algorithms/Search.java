16b242d7db0ee6152a52f1dd9b95d5d7e73d4574
Michael O'Brien <michael@obrienlabs.org>
2025-01-29 19:52:31 -0500

    #2 - set before/after

--- compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Search.java
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Search.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Search {
 	
@@ -69,7 +70,39 @@ public class Search {
 	  s = [2,7]  
 	  001000010
 	  */
-	  public long getMaxSparseFill(long N, long K, long M, int[] S) {
+	  public long getMaxSparseFill(int N, int K, int M, int[] S) {
+		  // data structure? - use .filter (convert Array to List)
+		  int[] array = new int[N];
+		  
+		  // pass 1 mark S
+		  //List<Integer> setList = Arrays.stream(S).boxed().collect(Collectors.toList());
+		  Arrays.stream(S).forEach(n -> {
+			  array[n - 1] = 1;
+			  // set before/after
+			  IntStream.rangeClosed(1, K).forEach(a -> {
+				  set(n - 1 - a, array);
+				  set(n - 1 + a, array);
+			  });
+		  });
+		  long initialCount = Arrays.stream(array).count();
+		  System.out.println("Before: " + initialCount);
+
+		  // pass 2 mark 0 on spacing
+	
+		  // pass 3 not required - mark ends >= 0
+		  // pass 4 iterate remaining - skip K
+		  Arrays.stream(array).forEach(System.out::println);
+		  
+		  return 0L;
+	  }
+	  
+	  private void set(int pos, int[] array) {
+		  if(pos >= 0 && pos < array.length) {
+			  array[pos] = 2;
+		  }
+	  }
+	  
+	  public long getMaxSparseFill_old1(long N, long K, long M, int[] S) {
 		  // data structure? - use .filter (convert Array to List)
 		  int[] array = new int[10];
 		  
@@ -97,6 +130,6 @@ public class Search {
 		//int[][] array = {{0,0,1},{1,0,0}};
 		//search.filledCellsPercentage(2, 3, array );
 		int[] array = {2,6};
-		System.out.println("Result: " + search.getMaxSparseFill(10L,1L,2L, array));
+		System.out.println("Result: " + search.getMaxSparseFill(10,1,2, array));
 	}
 }
