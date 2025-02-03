5ef99414f05f31b370960de8873c9e42659dcdaa
Michael O'Brien <michael@obrienlabs.org>
2025-02-03 16:52:29 -0500

    #2 - deque removal

--- compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Ordering.java
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/Ordering.java
@@ -1,9 +1,32 @@
 package dev.obrienlabs.compsci.algorithms;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.stream.Collectors;
+
 public class Ordering {
 	
 	public int[] reverse(int[] a) {
-		return a;
+		// built in via StringBuilder
+		// using a stack - no - use deque
+		Deque<Integer> queue = new ConcurrentLinkedDeque<>();
+		//List<Integer> list = Arrays.stream(a).boxed().collect(Collectors.toList());
+		Arrays.stream(a).forEach(x -> queue.add(x));
+		ArrayList<Integer> list = new ArrayList<>();
+		queue.stream().forEach(x -> list.add(queue.removeLast()));
+		// 1 element remaining
+		list.add(queue.removeLast());
+		//int[] typedArray = new Integer[list.size()];
+		//typedArray = list.toArray();
+		//return typedArray;
+		Integer[] typedArray = list.stream().toArray(Integer[]::new);
+		return Arrays.stream(typedArray).mapToInt(x -> x).toArray();
 	}
 	
 	public boolean same(int[] a, int[] b) {
