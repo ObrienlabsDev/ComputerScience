64d5690fa3da393616e4b43a7efaa0e36415c8ac
Michael O'Brien <michael@obrienlabs.org>
2025-03-09 22:54:53 -0400

    #2 - Create primes.java

---
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/algorithms/primes.java
@@ -0,0 +1,24 @@
+package dev.obrienlabs.compsci.algorithms;
+
+import java.util.stream.IntStream;
+
+public class Primes<T extends Number> {
+	
+	// 1,1,2,3,5,8.13
+	public Integer getFibonacciNumber(Integer number) {
+		// base
+		if(number < 2) {
+			return Integer.valueOf(1);
+		} else {
+		// recursive
+			return getFibonacciNumber(number - 1) + getFibonacciNumber(number - 2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Fibonacci<Integer> f = new Fibonacci<>();
+		IntStream.rangeClosed(0,15).forEach(
+			i -> System.out.println(i + " = " + f.getFibonacciNumber(Integer.valueOf(i))));
+
+	}
+}
