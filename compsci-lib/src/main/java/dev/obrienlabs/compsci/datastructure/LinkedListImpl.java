ad06c2eda002278837dd7fe815218ceae7df9eea
Michael O'Brien <michael@obrienlabs.org>
2025-07-16 13:51:37 -0400

    #1 - by reference

--- compsci-lib/src/main/java/dev/obrienlabs/compsci/datastructure/LinkedListImpl.java
+++ compsci-lib/src/main/java/dev/obrienlabs/compsci/datastructure/LinkedListImpl.java
@@ -1,26 +1,31 @@
 package dev.obrienlabs.compsci.datastructure;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.LongStream;
 
 public class LinkedListImpl {
 
 	LinkedList<Long> list;
 	
-	
-	public void uc1InsertionOptimized() {
-		long start = System.nanoTime();
-		
+	public LinkedListImpl() {
 		list = new LinkedList<>();
-		LongStream.range(0, 1 << 31)
-			.forEach(i -> list.add(i));
-		
-		// randomly remove items
-		LongStream.range(0, 1 << 11)
-		.forEach(i -> list.remove(i));
+	}
 	
-		long end = System.nanoTime();
-		System.out.println("Dur NS: " + (end - start));
+	public void uc1InsertionOptimized(List<Long> aList, Long p1) {
+		long start = System.currentTimeMillis();//.nanoTime();
+		p1 = Long.valueOf(2L);
+		
+		LongStream.range(0, 1 << 4)//27)
+			.forEach(i -> aList.add(i));
+		
+		System.out.println("pass by reference size in function: " + aList.size());
+		// randomly remove items
+		//LongStream.range(0, 1 << 11)
+		//.forEach(i -> aList.remove(i));
+	
+		long end = System.currentTimeMillis();//.nanoTime();
+		System.out.println("Dur MS: " + (end - start));
 	}
 	
 	public void uc2SearchSubOptimial() {
@@ -29,9 +34,12 @@ public class LinkedListImpl {
 	
 	public static void main(String[] args) {
 		LinkedListImpl list = new LinkedListImpl();
-		list.uc1InsertionOptimized();
+		LinkedList<Long> aList = new LinkedList<>();
+		aList.add(Long.valueOf(0L));
+		Long param = Long.valueOf(1L);
+		list.uc1InsertionOptimized(aList, param);
 		list.uc2SearchSubOptimial();
-
+		System.out.println("pass by reference size in caller: " + aList.size());
 	}
 
 }
