be3c33e50b0560e3745c06e2e4763dd6735a368b
Michael O'Brien <michael@obrienlabs.org>
2025-08-03 21:02:29 -0400

    #9 - dictionary enumerate()

--- compsci-lib-python/python/scratch.py
+++ compsci-lib-python/python/scratch.py
@@ -1,9 +1,15 @@
 
 # tuples
+aTuple = {4, 3, 5, 2, 1}
+# add ids
+enumerated = enumerate(aTuple)
+# create dictionary
+vocab = {token:integer for integer,token in enumerated} 
+# {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
+print(vocab)
 
-aTuple = {"first", "second", "third"}
-print(aTuple)
+# find all condos with a view to the ocean on the right
+# work backwards and iterate the list - an O(n) operation
 
-# find all condos with a view to the ocean
 
 
