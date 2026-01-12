99416edb34b7f2cc8d17bc41e85d143bc97cbc00
Michael O'Brien <michael@obrienlabs.org>
2026-01-11 19:58:02 -0500

    #2 - list via range

--- compsci-lib-python/python/scratch.py
+++ compsci-lib-python/python/scratch.py
@@ -6,13 +6,18 @@ enumerated = enumerate(aTuple)
 # create dictionary
 vocab = {token:integer for integer,token in enumerated} 
 # {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
-print(vocab)
+#print(vocab)
 
 # find all condos with a view to the ocean on the right
 # work backwards and iterate the list - an O(n) operation
 
 
-aList = [1,5,2,8,4,7,6]
-aList.sort()
-for i in aList:
+#aList = [1,5,2,8,4,7,6]
+#aList.sort()
+#for i in aList:
+#  print(i)
+
+value = 1
+aList2 = [ value**2 for value in range(5)]
+for i in aList2:
   print(i)
