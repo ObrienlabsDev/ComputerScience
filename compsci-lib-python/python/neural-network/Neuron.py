f0fa17b563990fd85b267af086cf83796ed58e88
Michael O'Brien <michael@obrienlabs.org>
2026-02-06 18:52:17 -0500

    #4 - classes

--- compsci-lib-python/python/neural-network/Neuron.py
+++ compsci-lib-python/python/neural-network/Neuron.py
@@ -1,6 +1,24 @@
+# https://cs.stanford.edu/people/eroberts/courses/soco/projects/neural-networks/Neuron/index.html
 class Neuron:
     value = 1
+    activationFunction = 1
+    bias = 0
+    weightedSumOfInputs = 1
+    inputVector = 1
+    weightVector = 1
 
-neuron = Neuron()
-print(neuron.value)
+    # default constructor
+
+    def __init__(this, bias, value=0):
+        this.bias = bias
+        this.value = value
+
+    def printOn(this):
+        print(f"{this.bias},{this.value}")
+
+
+
+neuron = Neuron(2)
+#print(neuron.bias, neuron.value)
+neuron.printOn()
 
--- compsci-lib-python/python/scratch.py
+++ compsci-lib-python/python/scratch.py
@@ -6,7 +6,7 @@ enumerated = enumerate(aTuple)
 # create dictionary
 vocab = {token:integer for integer,token in enumerated} 
 # {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
-#print(vocab)
+print(vocab)
 
 # find all condos with a view to the ocean on the right
 # work backwards and iterate the list - an O(n) operation
